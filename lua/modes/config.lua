d27a8ed529eb26ecc4a3dadf2ff4f39fb3e39e96
fitrh <fitrahm17h@student.unhas.ac.id>
2021-09-16 13:47:53 +0800

    feat: add option to set line highlight opacity per mode
    Make the `line_opacity` option to accept table with the same form of
    `colors` option as it value, where the table describes line highlight
    opacity for each mode.
    
    Passing a table with incomplete mode will only affect that passed mode
    and the rest will use the default values.

--- lua/modes/config.lua
+++ lua/modes/config.lua
@@ -7,7 +7,12 @@ M.default = {
 		insert = '#78ccc5',
 		visual = '#9745be',
 	},
-	line_opacity = 0.15,
+	line_opacity = {
+		copy = 0.15,
+		delete = 0.15,
+		insert = 0.15,
+		visual = 0.15,
+	},
 	focus_only = false,
 }
 
